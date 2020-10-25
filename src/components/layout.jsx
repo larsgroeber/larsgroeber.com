84356776467ff620b038a50174c408e3fd71f1be
Lars Gröber <lars.groeber@inheaden.io>
2020-10-25 22:19:01 +0100

    init

---
+++ src/components/layout.jsx
@@ -0,0 +1,66 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import ProfileImage from "../images/profile_full.jpg"
+import "./layout.scss"
+
+const Layout = ({ children }) => {
+  const [showBanner, setShowBanner] = useState(true)
+
+  return (
+    <div className="layout">
+      <div className="profile-container">
+        <img src={ProfileImage} alt="profile" />
+      </div>
+      <main>
+        <div className="main-inner-wrapper">{children}</div>
+        <footer>
+          <Link to="/imprint">Imprint</Link>
+          <Link to="/privacy">Dataprivacy</Link>
+        </footer>
+      </main>
+      {showBanner && (
+        <div className="banner">
+          <div>
+            <p>
+              I am not storing any data about you or your visit of my website.
+              Also no fonts or external scripts are loaded or cookies saved (
+              <a
+                href="https://github.com/larsgroeber/larsgroeber.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                check out the source
+              </a>
+              ).
+            </p>
+            <p>
+              My hoster might store your IP address and/or other information
+              about you (
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                see their policy
+              </a>
+              ).
+            </p>
+          </div>
+          <div>
+            <button onClick={() => setShowBanner(false)}>Got it</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.jsx
@@ -0,0 +1,80 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "./index.scss"
+
+const links = [
+  {
+    name: "Podcast with StartupFever",
+    link: "https://startupfever.de/lars-groeber/",
+  },
+  {
+    name: "Interview with Citrus Global (Youtube)",
+    link: "https://www.youtube.com/watch?v=fXRVv3kRdQ0",
+  },
+]
+
+const socal = [
+  {
+    icon: require("../images/linkedin.svg"),
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/larsgroeber",
+  },
+  {
+    icon: require("../images/github.svg"),
+    name: "github",
+    link: "https://github.com/larsgroeber",
+  },
+  {
+    icon: require("../images/twitter.svg"),
+    name: "twitter",
+    link: "https://twitter.com/GroeberLars",
+  },
+]
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <div className="content">
+      <h1>Lars Gröber</h1>
+      <p className="subtitle">
+        CTO & Co-Founder of{" "}
+        <a href="http://inheaden.io" target="_blank" rel="noopener noreferrer">
+          Inheaden
+        </a>
+      </p>
+
+      <div className="links">
+        {links.map(l => (
+          <p key={l.name}>
+            <a href={l.link} target="_blank" rel="noopener noreferrer">
+              {l.name}
+            </a>
+          </p>
+        ))}
+      </div>
+
+      <div
+        className="social-media"
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${socal.length}, 40px)`,
+        }}
+      >
+        {socal.map(s => (
+          <a
+            key={s.name}
+            href={s.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={s.icon} alt={s.name} />
+          </a>
+        ))}
+      </div>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
